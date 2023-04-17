46b11285e2aa617a90282c29f80866cb25b860fd
Battu Sharath <sharathbattu123987@gmail.com>
2023-04-17 03:47:47 +0000

    using javascript other files accessed

--- node.js
+++ node.js
@@ -1,4 +1,13 @@
 
-let os= require("os");
-console.log(os.arch());
-console.log(os.platform());
+// using os module-----------------------------------------------------
+// let os= require("os");
+// console.log(os.arch());
+// console.log(os.platform());
+
+// using childprocess---------------------------------------------------------
+let variable=require("child_process");
+// console.log("opening calculator");
+// let ans=variable.execSync("code");
+
+let ans2=variable.execSync("node abc.js");// another file oprning
+console.log(""+ans2)   // for strings use ""+ otherwise the output in tthe binary form
---
+++ println.java
@@ -0,0 +1,5 @@
+public class println{
+    public static void main(String []args){
+          System.out.println("using javascript  run java file using child process ");
+    }
+}
